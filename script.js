01113bc87ce7963c2e6a8b13d5b977e1d511d1e5
yannsecundo <yannsecundo34@gmail.com>
2022-05-05 19:04:16 -0400

    todos requisitos finais atendidos

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const email = document.querySelector('.login-email');
 const senha = document.querySelector('.login-senha');
 const botao = document.querySelector('#botao');
+const submitBtn = document.querySelector('#submit-btn');
+const checkboxAgreement = document.querySelector('#agreement');
+window.onload = () => {
+  submitBtn.disabled = true;
+};
 botao.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkboxAgreement.addEventListener('click', () => {
+  if (checkboxAgreement.value === true) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
   }
 });
