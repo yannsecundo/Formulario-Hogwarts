6feef91761ca1c4f6b3c5784069e7403a938ec21
Andre Luis Ribeiro de Souza <101289554+andre09999@users.noreply.github.com>
2022-05-05 20:04:18 -0300

    contador feito

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const email = document.querySelector('.login-email');
 const senha = document.querySelector('.login-senha');
 const botao = document.querySelector('#botao');
+const desc = document.querySelector('#textarea');
+const cont = document.querySelector('#counter');
 botao.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+desc.addEventListener('keypress', () => {
+  const tam = desc.value.length + 1;
+  cont.innerHTML = 500 - tam;
+  console.log(tam);
 });
